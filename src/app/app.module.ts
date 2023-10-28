ad5fad18a981cfcf36d13dabc5833f9f6570f82c
Yevhenii Danoi <geneforapp@gmail.com>
2023-10-28 09:33:12 +0300

    adding filters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatRadioModule } from '@angular/material/radio';
 
 import { AppComponent } from './app.component';
 import { CatsComponent } from './components/cats/cats.component';
 import { CatsInterceptor } from './interceptors/cats.interceptor';
+import { FiltersComponent } from './components/filters/filters.component';
 
 @NgModule({
-  declarations: [AppComponent, CatsComponent],
-  imports: [BrowserModule, HttpClientModule, MatGridListModule],
+  declarations: [AppComponent, CatsComponent, FiltersComponent],
+  imports: [BrowserModule, HttpClientModule, MatGridListModule, MatRadioModule],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
